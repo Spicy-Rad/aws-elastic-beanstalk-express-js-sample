4f6030202447b3acb3a71c30eaf206ea1ad8f0a4
Tyler Burke <jamesonetwelve@gmail.com>
2021-04-26 13:25:00 -0700

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('chicken strips'));
+app.get('/', (req, res) => res.send('Fuck yo chicken strips, this is a test biiiiiiiiitch'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
